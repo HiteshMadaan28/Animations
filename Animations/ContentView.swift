7b16179ded5b0e18753e2d8c10dcb94ae95dd653
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-05-13 23:57:03 +0530

    using overlay function

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -10,20 +10,27 @@ import SwiftUI
 struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
-        Button("Tap Me"){
-            //Body of Button
-            animationAmount += 0.5
+        Button("Tap Me") {
+            // animationAmount += 1
         }
         .padding(50)
         .background(.red)
-        .foregroundColor(.white)
-        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(
-            .easeInOut(duration: 2)
-            .delay(2),
-            value: animationAmount)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        .onAppear {
+            animationAmount = 2
+        }
         
     }
 }
