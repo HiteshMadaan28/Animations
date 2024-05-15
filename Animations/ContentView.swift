124e562c94b751c46a6442bd440fb287bb28a7a1
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-05-15 23:19:19 +0530

    implicit animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -10,26 +10,31 @@ import SwiftUI
 struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
-        Button("Tap Me") {
-            // animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeInOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
+        print(animationAmount)
+        return VStack{
+            Stepper("Click here",value: $animationAmount.animation(),in: 1...10)
+                .frame(width: 200,height: 200)
+            Button("Tap Me") {
+                 animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .overlay(
+                Circle()
+                    .stroke(.pink)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation(
+                        .easeInOut(duration: 2)
+                            .repeatForever(autoreverses: false),
+                        value: animationAmount
+                    )
+            )
+            .onAppear {
+                animationAmount = 2
+            }
         }
         
     }
