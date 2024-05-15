b6874a945e3452294fdb2c2d38601bbd80d202d5
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-05-15 23:28:19 +0530

    Explicit Animation with 3d effect

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,33 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
     var body: some View {
         print(animationAmount)
         return VStack{
-            Stepper("Click here",value: $animationAmount.animation(),in: 1...10)
-                .frame(width: 200,height: 200)
+//            Stepper("Click here",value: $animationAmount.animation(),in: 1...10)
+//                .frame(width: 200,height: 200)
+//            Button("Tap Me") {
+//                 animationAmount += 1
+//            }
+//            .padding(50)
+//            .background(.red)
+//            .foregroundStyle(.white)
+//            .clipShape(.circle)
+//            .overlay(
+//                Circle()
+//                    .stroke(.pink)
+//                    .scaleEffect(animationAmount)
+//                    .opacity(2 - animationAmount)
+//                    .animation(
+//                        .easeInOut(duration: 2)
+//                            .repeatForever(autoreverses: false),
+//                        value: animationAmount
+//                    )
+//            )
+//            .onAppear {
+//                animationAmount = 2
+//            }
+            
             Button("Tap Me") {
-                 animationAmount += 1
+                withAnimation {
+                    animationAmount += 90
+                }
             }
-            .padding(50)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-            .overlay(
-                Circle()
-                    .stroke(.pink)
-                    .scaleEffect(animationAmount)
-                    .opacity(2 - animationAmount)
-                    .animation(
-                        .easeInOut(duration: 2)
-                            .repeatForever(autoreverses: false),
-                        value: animationAmount
-                    )
-            )
-            .onAppear {
-                animationAmount = 2
-            }
+                    .padding(50)
+                    .background(.red)
+                    .foregroundStyle(.white)
+                    .clipShape(.circle)
+                    .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 1, z: 1))
         }
         
     }
